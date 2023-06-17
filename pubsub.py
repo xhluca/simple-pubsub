f88b74e0dd30332915e2223eec76407ee41132df
Xing Han Lu <21180505+xhluca@users.noreply.github.com>
2023-06-16 23:58:16 -0400

    Improve pubsub.py

--- pubsub.py
+++ pubsub.py
@@ -1,3 +1,4 @@
+from functools import partial
 from multiprocessing import Process, Queue
 from uuid import uuid4
 
@@ -18,17 +19,14 @@ class Pubsub:
         self.listener_queue = Queue()
         self.uid = str(uuid4())
     
-    def _put_message(self, channel: str):
-        def put(message):
-            self.listener_queue.put({'channel': channel, 'data': message.encode(), 'type': 'message'})
-        
-        return put
+    def _put_message(self, channel: str, message: str):
+        return self.listener_queue.put({'channel': channel, 'data': message.encode(), 'type': 'message'})
     
     def subscribe(self, *channels):
+        self.unsubscribe(*channels)  # Remove previous subscriptions
         for channel in channels:
-            self.listeners[channel] = Listener(
-                self.manager.create_queue(channel, self.uid), self._put_message(channel)
-            )
+            q = self.manager.create_queue(channel, self.uid)
+            self.listeners[channel] = Listener(q, partial(self._put_message, channel))
             self.listeners[channel].start()
     
     def unsubscribe(self, *channels):
@@ -37,13 +35,13 @@ class Pubsub:
                 self.listeners.pop(channel).terminate()
                 self.manager.delete_queue(channel, self.uid)
 
-    def listen(self):
+    def listen(self, block=True, timeout=None):
         while True:
-            yield self.listener_queue.get()
+            yield self.listener_queue.get(block=block, timeout=timeout)
 
-def publish(manager, channel, message, block=False):
+def publish(manager, channel, message, block=False, timeout=None):
     for subkey in manager.get_subkeys(channel).copy():
-        manager.get_queue(channel, subkey).put(message, block=block)
+        manager.get_queue(channel, subkey).put(message, block=block, timeout=timeout)
 
 def load_manager(host="localhost", port=40080, authkey="abracadabra", register=True, connect=True):
     from multiprocessing.managers import BaseManager
